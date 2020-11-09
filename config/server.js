a1770152c249b19569821e374d9b0b3150e6eed4
Sarah-Burnett <burnie8657@gmail.com>
2020-11-09 16:48:12 +0000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9d5f043c775c421b03232beee185fb11'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ea13d739-b702-4d53-b1c9-e2a69cc9302b'
+};
